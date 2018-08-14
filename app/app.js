9d5687acf1ba57441f824f057d75d24e78d24125
Kamen Bundev <bundyo@gmail.com>
2018-08-14 17:11:46 +0300

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,15 @@
+import './vendor';
+
+import Vue from 'nativescript-vue';
+
+import HelloWorld from './components/HelloWorld';
+
+// Uncommment the following to see NativeScript-Vue output logs
+Vue.config.silent = false;
+Vue.config.debug = true;
+
+new Vue({
+
+    render: h => h(HelloWorld),
+
+}).$start();
---
+++ app/components/HelloWorld.vue
@@ -0,0 +1,69 @@
+<template>
+	<Page xmlns="http://www.nativescript.org/tns.xsd" loaded="onPageLoaded">
+		<ActionBar>
+			<ActionBar title="Gradients" />
+		</ActionBar>
+		<ScrollView>
+			<StackLayout>
+				<StackLayout class="bottom-gradient" height="200" marginTop="16">
+					<Label text="background: linear-gradient(to bottom, orangered, green, lightblue);" textWrap="true" />
+				</StackLayout>
+				<StackLayout class="left-gradient" height="200" marginTop="16">
+					<Label text="background: linear-gradient(to left, orangered, green, lightblue);" textWrap="true" />
+				</StackLayout>
+				<StackLayout class="right-gradient" height="200" marginTop="16">
+					<Label text="background: linear-gradient(to right, orangered, green, lightblue);" textWrap="true" />
+				</StackLayout>
+				<StackLayout class="degree-gradient" height="200" marginTop="16">
+					<Label text="background: linear-gradient(45deg, orangered, green, lightblue);" textWrap="true" />
+				</StackLayout>
+				<StackLayout class="two-color-gradient" height="200" marginTop="16">
+					<Label text="background: linear-gradient(-45deg, orangered, lightblue);" textWrap="true" />
+				</StackLayout>
+				<StackLayout class="background-image" height="200" marginTop="16">
+					<Label text="background-image:linear-gradient(to bottom, orangered, green, lightblue);" textWrap="true" />
+				</StackLayout>
+			</StackLayout>
+		</ScrollView>
+	</Page>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+        };
+    },
+}
+</script>
+
+<style scoped>
+Label {
+    padding: 8;
+    color:white;
+}
+
+.bottom-gradient {
+    background: linear-gradient(to bottom, orangered, green, lightblue);
+}
+
+.left-gradient {
+    background: linear-gradient(to left, orangered, green, lightblue);
+}
+
+.right-gradient {
+    background: linear-gradient(to right, orangered, green, lightblue);
+}
+
+.degree-gradient {
+    background: linear-gradient(45deg, orangered, green, lightblue);
+}
+
+.two-color-gradient {
+    background: linear-gradient(-45deg, orangered, lightblue);
+}
+
+.background-image {
+    background-image:linear-gradient(to bottom, orangered, green, lightblue);
+}
+</style>
